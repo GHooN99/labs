5db667a4bb030c048ecce36f41fe32ad803627d1
ghoon99 <ghoon99@kakao.com>
2023-07-04 22:10:22 +0900

    config: add lint-staged,tsconfig workspace

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config} */
+const config = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  jsxSingleQuote: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
+
+module.exports = config;
